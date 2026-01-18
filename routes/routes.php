a2bf79b908c0cf3edd36228b3cdc5e658be83f7c
Enrique Sousa <enrique.sousa@gmail.com>
2026-01-18 10:16:35 -0800

    26. Configurar archivo .htaccess

--- routes/routes.php
+++ routes/routes.php
@@ -1,10 +1,27 @@
 <?php
- 
+
+$routesArray = explode("/", $_SERVER['REQUEST_URI']);
+$routesArray = array_filter($routesArray);
+
+// **************************************************************
+// Cuando no se hace ninguna petición a la API
+// **************************************************************
+if(count($routesArray) == 0){
+
+	$json = array(
+		'status' => 404,
+		'results' => 'Not Found'
+	);
+	echo json_encode($json, http_response_code($json["status"]));
+	return;
+}
+
+
+
 $json = array(
     'status' => 200,
     'result' => 'success'
 );
- 
 echo json_encode($json);
 
 return;
