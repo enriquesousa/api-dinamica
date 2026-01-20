6c22cfb88e55f06b86b10957a23cce4f2f8c32f4
Enrique Sousa <enrique.sousa@gmail.com>
2026-01-19 19:23:50 -0800

    27. Habilitar servicios HTTP GET, POST, PUT, DELETE

--- routes/routes.php
+++ routes/routes.php
@@ -16,12 +16,90 @@ if(count($routesArray) == 0){
 	return;
 }
 
+// $json = array(
+//     'status' => 200,
+//     'result' => 'success'
+// );
+// echo json_encode($json);
+// return;
 
 
-$json = array(
-    'status' => 200,
-    'result' => 'success'
-);
-echo json_encode($json);
+// **************************************************************
+// Cuando si se hace una petición a la API
+// **************************************************************
+if(count($routesArray) == 1 && isset($_SERVER['REQUEST_METHOD'])){
 
-return;
+    echo '<pre>'; print_r($_SERVER['REQUEST_METHOD']); echo '</pre>';
+
+	// $table = explode("?", $routesArray[1])[0];
+
+	/*=============================================
+	Validar llave secreta
+	=============================================*/
+	// if(!isset(getallheaders()["Authorization"]) || getallheaders()["Authorization"] != Connection::apikey()){
+	// 	if(in_array($table, Connection::publicAccess()) == 0){
+	// 		$json = array(
+	// 			'status' => 400,
+	// 			"results" => "You are not authorized to make this request"
+	// 		);
+	// 		echo json_encode($json, http_response_code($json["status"]));
+	// 		return;
+	// 	}else{
+	// 		/*=============================================
+	// 		Acceso público
+	// 		=============================================*/
+	// 		$response = new GetController();
+	// 		$response -> getData($table, "*",null,null,null,null);
+	// 		return;
+	// 	}
+	// }
+
+	/*=============================================
+	Peticiones GET
+	=============================================*/
+	if($_SERVER['REQUEST_METHOD'] == "GET"){
+        $json = array(
+            'status' => 200,
+            'result' => 'Solicitud GET'
+        );
+        echo json_encode($json);
+		// include "services/get.php";
+	}
+
+	/*=============================================
+	Peticiones POST
+	=============================================*/
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $json = array(
+            'status' => 200,
+            'result' => 'Solicitud POST'
+        );
+        echo json_encode($json);
+		// include "services/post.php";
+	}
+
+	/*=============================================
+	Peticiones PUT
+	=============================================*/
+	if($_SERVER['REQUEST_METHOD'] == "PUT"){
+        $json = array(
+            'status' => 200,
+            'result' => 'Solicitud PUT'
+        );
+        echo json_encode($json);
+		// include "services/put.php";
+	}
+
+	/*=============================================
+	Peticiones DELETE
+	=============================================*/
+	if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+        $json = array(
+            'status' => 200,
+            'result' => 'Solicitud DELETE'
+        );
+        echo json_encode($json);
+		// include "services/delete.php";
+	}
+
+}
